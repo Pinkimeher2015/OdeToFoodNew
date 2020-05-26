0a150613125a52a3f2ec16e234e0ac4abe931458
Prasanta Kumar <prasanta.kumar.meher@hp.com>
2020-05-26 11:25:01 +0530

    Initial push

---
+++ OdeToFood.Data/Models/coisine_type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    public enum coisine_type
+    {
+        None,
+        Italian,
+        Chinese,
+        Indian,
+        French
+    }
+}
---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurants> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurants>()
+            {
+                new Restaurants{Id=1, Name="KFC Chicken" , Couisine = coisine_type.French},
+                new Restaurants{Id=1, Name="Burgers" , Couisine = coisine_type.Indian},
+                new Restaurants{Id=1, Name="Hakka Noodles" , Couisine = coisine_type.Chinese},
+            };
+        }
+        IEnumerable<Restaurants> IRestaurantData.GetAll()
+        {
+            return restaurants.OrderBy(p => p.Name);
+           
+        }
+    }
+}
